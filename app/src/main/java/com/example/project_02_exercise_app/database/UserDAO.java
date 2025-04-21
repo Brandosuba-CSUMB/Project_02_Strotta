4054e49c734b6b4cc3fb0c12493c3f9d97ba782f
Brandosuba-CSUMB <bcalvario@csumb.edu>
2025-04-20 21:23:49 -0700

    Added Room and LiveData dependencies, modified login page, and UserDAO can now be implemented.

--- app/src/main/java/com/example/project_02_exercise_app/database/UserDAO.java
+++ app/src/main/java/com/example/project_02_exercise_app/database/UserDAO.java
@@ -1,5 +1,8 @@
 package com.example.project_02_exercise_app.database;
+import androidx.room.Dao;
 
 import com.example.project_02_exercise_app.database.entities.User;
+@Dao
 public interface UserDAO {
+
 }
