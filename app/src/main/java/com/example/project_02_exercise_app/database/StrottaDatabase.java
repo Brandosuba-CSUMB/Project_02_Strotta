bba292c27a35cbb71e6aacaede8aa566e2eb16f1
Kinsey-Lathbury <klathbury@csumb.edu>
2025-04-20 23:21:11 -0700

    Edited StrottaDatabase.java

--- app/src/main/java/com/example/project_02_exercise_app/database/StrottaDatabase.java
+++ app/src/main/java/com/example/project_02_exercise_app/database/StrottaDatabase.java
@@ -9,12 +9,13 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import com.example.project_02_exercise_app.database.entities.Strotta;
 import com.example.project_02_exercise_app.database.entities.User;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {User.class}, version = 1, exportSchema = false)
+@Database(entities = {Strotta.class, User.class}, version = 2, exportSchema = false)
 public abstract class StrottaDatabase extends RoomDatabase {
 
     public static final String USER_TABLE = "usertable";
