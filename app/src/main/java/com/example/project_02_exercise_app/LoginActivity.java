437356fd1af287fe98fdf757fe3508ffb29dd742
Abel <abelp1818@gmail.com>
2025-04-21 01:29:40 -0700

    method linked and fixed

--- app/src/main/java/com/example/project_02_exercise_app/LoginActivity.java
+++ app/src/main/java/com/example/project_02_exercise_app/LoginActivity.java
@@ -8,8 +8,6 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 
-import com.example.project_02_exercise_app.database.StrottaRepository;
-
 import com.example.project_02_exercise_app.database.entities.User;
 import com.example.project_02_exercise_app.databinding.ActivityLoginBinding;
 
