47d1956e77b253b30f0cc5ec8ca199a26afe2df4
Kinsey-Lathbury <klathbury@csumb.edu>
2025-04-21 00:04:24 -0700

    Tried to make the verifyUser() method, but ran into trouble.

--- app/src/main/java/com/example/project_02_exercise_app/LoginActivity.java
+++ app/src/main/java/com/example/project_02_exercise_app/LoginActivity.java
@@ -6,13 +6,16 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
 
+import com.example.project_02_exercise_app.database.StrottaRepository;
 import com.example.project_02_exercise_app.database.entities.User;
 import com.example.project_02_exercise_app.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
+
+    private StrottaRepository repository;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -30,26 +33,34 @@ public class LoginActivity extends AppCompatActivity {
     private void verifyUser() {
 
         String username = binding.userNameLoginEditText.getText().toString();
+        String password = binding.passswordLoginEditText.getText().toString();
 
         if (username.isEmpty()) {
             toastMaker("Username may not be blank.");
             return;
         }
-        LiveData<User> userObserver = repository.getUserByUserName(username);
-        userObserver.observe(this, user -> {
-            if (user != null) {
-                String password = binding.passswordLoginEditText.getText().toString();
-                if (password.equals(user.getPassword())) {
-                    startActivity(LandingActivity.landingActivityIntentFactory(getApplicationContext(), user.getId()));
-                } else {
-                    toastMaker("Invalid password");
-                    binding.passswordLoginEditText.setSelection(0);
-                }
-            } else {
-                toastMaker(String.format("%s is not a valid username.", username));
-                binding.userNameLoginEditText.setSelection(0);
-            }
-        });
+
+        if (password.isEmpty()) {
+            toastMaker("Password may not be blank.");
+            return;
+        }
+
+
+//        LiveData<User> userObserver = repository.getUserByUserName(username);
+//        userObserver.observe(this, user -> {
+//            if (user != null) {
+//                String password = binding.passswordLoginEditText.getText().toString();
+//                if (password.equals(user.getPassword())) {
+//                    startActivity(LandingActivity.landingActivityIntentFactory(getApplicationContext(), user.getId()));
+//                } else {
+//                    toastMaker("Invalid password");
+//                    binding.passswordLoginEditText.setSelection(0);
+//                }
+//            } else {
+//                toastMaker(String.format("%s is not a valid username.", username));
+//                binding.userNameLoginEditText.setSelection(0);
+//            }
+//        });
     }
 
     private void toastMaker(String message) {
