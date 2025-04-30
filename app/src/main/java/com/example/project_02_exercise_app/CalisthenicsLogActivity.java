15a1249e63ae24382e8a265d4d1a7065e4941fd1
Kinsey-Lathbury <klathbury@csumb.edu>
2025-04-30 11:50:47 -0700

    Started working on CalisthenicsLogActivity

--- app/src/main/java/com/example/project_02_exercise_app/CalisthenicsLogActivity.java
+++ app/src/main/java/com/example/project_02_exercise_app/CalisthenicsLogActivity.java
@@ -1,7 +1,11 @@
 package com.example.project_02_exercise_app;
 
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CalisthenicsLogActivity extends AppCompatActivity {
+    private long elapsed;
+    private TextView logHistoryTextView;
 
 }
