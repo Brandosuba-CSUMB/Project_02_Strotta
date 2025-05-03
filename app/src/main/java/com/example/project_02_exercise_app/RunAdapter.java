58fac5f77d1373672f4d0fbda6367eaf1a2080c0
Brandosuba-CSUMB <bcalvario@csumb.edu>
2025-05-02 20:42:37 -0700

    admin now displays recycler view and logs, can delete all logs as well,

--- app/src/main/java/com/example/project_02_exercise_app/RunAdapter.java
+++ app/src/main/java/com/example/project_02_exercise_app/RunAdapter.java
@@ -19,8 +19,7 @@ public class RunAdapter extends RecyclerView.Adapter<RunAdapter.RunViewHolder> {
     @NonNull
     @Override
     public RunViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.run_item,parent,false);
-        return new RunViewHolder(v);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.run_item, parent, false);        return new RunViewHolder(v);
     }
 
     @Override
